8649e3029aa2088297af84f997f5d433e02dc588
sanha53 <samantasanhasanji@gmail.com>
2025-05-01 01:49:25 +0600

    first half

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
- /** @type {import('tailwindcss').Config} */
- export default {
-    >   content: ["./src/**/*.{html,js}"],
-        theme: {
-          extend: {},
-        },
-        plugins: [],
-      }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"], // Paths to all template files using Tailwind classes
+  theme: {
+    extend: {}, // Custom theme extensions go here
+  },
+  plugins: [], // Add any Tailwind plugins here
+}
